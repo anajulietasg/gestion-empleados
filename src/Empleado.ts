9266c1edd5062e8a8ae38186e4bfe4d7cd0b5a22
Ana Julieta Sanchez <anajulietasg@gmail.com>
2026-09-10 20:11:12 -0300

    agrego superclase Empleado e interfaz IPagable

---
+++ src/Empleado.ts
@@ -0,0 +1,21 @@
+import { IPagable } from "./IPagable";
+
+export abstract class Empleado implements IPagable {
+    private _nombre: string;
+    private _legajo: number;
+
+    constructor(nombre: string, legajo: number) {
+        this._nombre = nombre;
+        this._legajo = legajo;
+    }
+    get nombre(): string {
+        return this._nombre;
+    }
+    get legajo(): number {
+        return this._legajo;
+    }
+    abstract calcularSueldo(): number;
+    descripcion(): string {
+        return `Empleado: ${this._nombre} - Legajo: ${this._legajo}`;
+    }
+}
